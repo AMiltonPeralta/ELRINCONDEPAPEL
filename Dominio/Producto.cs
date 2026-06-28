8d6b7e8900400bb5f4a1314c0c13287084721a54
AMiltonPeralta <130006712+AMiltonPeralta@users.noreply.github.com>
2026-06-28 18:52:03 -0300

    dominio

--- Dominio/Producto.cs
+++ Dominio/Producto.cs
@@ -14,6 +14,7 @@ namespace Dominio
         public Marca Marca { get; set; }
         public Categoria Categoria { get; set; }
         public int StockActual { get; set; }
+        public decimal Precio { get; set; }
         public bool Activo { get; set; }
     }
 }
